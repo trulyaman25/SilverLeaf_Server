560f39cef55125ef4afeeb10238b5dd615274601
Aman Kumar <trulyaman25@gmail.com>
2025-02-02 12:06:00 +0530

    Cors Issue fixed

--- index.js
+++ index.js
@@ -7,10 +7,20 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+const allowedOrigins = ['http://localhost:5173', 'https://silverleafstudio.vercel.app'];
+
 app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST'],
-    credentials: true
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 connectDB();
